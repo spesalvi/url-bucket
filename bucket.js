f3fbbbd39ad8779c75741d395f5d74587ddbcf49
Robert George <robert@poovelil.org>
2011-11-08 18:18:18 +0530

    added basic indexdb support. display is incomplete. to be finished

--- bucket.js
+++ bucket.js
@@ -1,4 +1,102 @@
-var links = [];
+var urlbucket  = {},
+    links = [],
+    indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
+
+if ('webkitIndexedDB' in window) {
+	window.IDBTransaction = window.webkitIDBTransaction;
+	window.IDBKeyRange = window.webkitIDBKeyRange;
+}
+urlbucket.indexedDB = {};
+urlbucket.indexedDB.db = null;
+urlbucket.indexedDB.getAllUrls = function () {
+
+};
+
+urlbucket.indexedDB.open = function () {
+	var request = indexedDB.open("url_bucket",
+			"This is the offline version of your tweet-reader bookmarks.");
+
+	request.onsuccess = function (e) {
+		urlbucket.indexedDB.db = e.target.result;
+		var version = "1.0",
+		    db = urlbucket.indexedDB.db,
+		    setVrequest;
+
+		if (version !== db.version) {
+			setVrequest = db.setVersion(version);
+
+			setVrequest.onfailure = urlbucket.indexedDB.onerror;
+			setVrequest.onsuccess = function (e) {
+				var store = db.createObjectStore("urls",
+						{"keyPath" : "timeStamp"});
+			};
+		}
+		urlbucket.indexedDB.getAllUrls();
+	};
+
+	request.onfailure = urlbucket.indexedDB.onerror;
+};
+
+urlbucket.indexedDB.showBucketContents = function () {
+	var db = urlbucket.indexedDB.db,
+	    trans = db.transaction(["urls"], IDBTransaction.READ_WRITE, 0),
+	    store = trans.objectStore("urls"),
+	    keyRange = IDBKeyRange.lowerBound(0),
+	    cursorRequest = store.openCursor(keyRange),
+	    bucket = document.createElement('div'),
+	    link_list = document.createElement('ul');
+
+	cursorRequest.onsuccess = function (e) {
+		var result = e.target.result,
+		    link = document.createElement('li');
+		if (!!result === false) {
+			return;
+		}
+		link.innerHTML = result.value.link;
+		link_list.appendChild(link);
+		result.continue();
+	};
+	bucket.appendChild(link_list);
+	document.body.appendChild(bucket);
+};
+
+function updateUrlDisplayCount() {
+	var db = urlbucket.indexedDB.db,
+	    trans = db.transaction(["urls"], IDBTransaction.READ_WRITE, 0),
+	    store = trans.objectStore("urls"),
+	    keyRange = IDBKeyRange.lowerBound(0),
+	    cursorRequest = store.openCursor(keyRange),
+	    count = 0;
+
+	cursorRequest.onsuccess = function (e) {
+		var result = e.target.result;
+		if (!!result === false) {
+			return;
+		}
+		count += 1;
+		result.continue();
+	};
+	cursorRequest.onerror = urlbucket.indexedDB.onerror;
+}
+
+urlbucket.indexedDB.addUrl = function (link) {
+	var db = urlbucket.indexedDB.db,
+	    trans = db.transaction(["urls"], IDBTransaction.READ_WRITE, 0),
+	    store = trans.objectStore("urls"),
+	    request = store.put({
+			"link": link,
+			"timeStamp": new Date().getTime()
+	    });
+
+	request.onsuccess  = function (e) {
+		updateUrlDisplayCount();
+	};
+
+	request.onerror = function (e) {
+		console.log("failed to add link to indexDB object store." +
+				" Show a message using notifications api");
+	};
+};
 
 function handleDragEnter(e) {
 	this.style.border = "2px dotted black";
@@ -27,6 +125,7 @@ function handleDrop(e) {
 		e.stopPropagation();
 	}
 	links.push(droppedData);
+	urlbucket.indexedDB.addUrl(droppedData);
 	this.innerHTML = links.length + ' urls in bucket';
 	return false;
 }
@@ -60,8 +159,9 @@ function createBucket() {
 	div.style.padding = "2px";
 	div.style.height = "100px";
 	div.style.width = "100px";
-	div.style.backgroundColor = "gold";
+	div.style.backgroundColor = "#aaa";
 	div.style.zIndex = "1000";
+	div.style.webkitBorderRadius = "10px";
 
 	//element classname 
 	div.id = 'url_bucket';
@@ -73,14 +173,15 @@ function showBucketContent() {
 	var openBucket = document.createElement('div'),
 	    bucketContent = '';
 
-	links.forEach(function(link) {
-			bucketContent += link + '<br>';
-			});
+	links.forEach(function (link) {
+		bucketContent += link + '<br>';
+	});
 	openBucket.innerHTML = bucketContent;
 	document.body.appendChild(openBucket);
 }
 
 function init() {
+	urlbucket.indexedDB.open();
 	createBucket();
 	addEventListeners();
 }
